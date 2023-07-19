7cebefc623272d1273b407ecfe629f4154a7a159
Rohith <95613210+RohiV04@users.noreply.github.com>
2023-07-19 18:49:12 +0530

    first commit

--- Navigation.js
+++ Navigation.js
@@ -7,7 +7,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './Home.js';
 import Notification from './Notification.js';
 import LoginScreen from './Login.js';
-
+import { View, StyleSheet, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
 const Stack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
@@ -43,14 +45,21 @@ const BottomTab = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      activeColor="#fff"
-      barStyle={{ backgroundColor: '#9AC5F4' }}
+      tabBarOptions={{
+        style: { backgroundColor: 'transparent' },
+        showLabel: false,
+      }}
+      tabBar={(props) => (
+        <>
+          <CustomTabBarBackground />
+          <BottomTabNavigator {...props} />
+        </>
+      )}
     >
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarLabel: 'Home',
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="home" color={color} size={26} />
           ),
@@ -60,7 +69,6 @@ const BottomTab = () => {
         name="Notification"
         component={Notification}
         options={{
-          tabBarLabel: 'Updates',
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="bell" color={color} size={26} />
           ),
@@ -68,76 +76,17 @@ const BottomTab = () => {
       />
     </Tab.Navigator>
   );
-}
-
-// import React from 'react';
-// import { useSelector } from 'react-redux'; // Import useSelector from react-redux
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-// import { NavigationContainer } from '@react-navigation/native';
-// import Home from './Home.js';
-// import Notification from './Notification.js';
-// import LoginScreen from './Login.js';
-// import { setPhoneNumber } from './store.js';
-// const Stack = createStackNavigator();
-// const Tab = createMaterialBottomTabNavigator();
-
-// export default function Navigation() {
-//   // Get the phoneNumber state from the Redux store
-//   const phoneNumber = useSelector((state) => state.phoneNumber);
-
-//   // Check if the phoneNumber is set (not null)
-//   const isLoggedIn = phoneNumber !== null;
-
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         {isLoggedIn ? (
-//           <Stack.Screen
-//             name="Main"
-//             component={BottomTab}
-//             options={{ headerShown: false }}
-//           />
-//         ) : (
-//           <Stack.Screen
-//             name="Login"
-//             component={LoginScreen}
-//             options={{ headerShown: false }}
-//           />
-//         )}
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// const BottomTab = () => {
-//   return (
-//     <Tab.Navigator
-//       initialRouteName="Home"
-//       activeColor="#fff"
-//       barStyle={{ backgroundColor: 'blue' }}
-//     >
-//       <Tab.Screen
-//         name="Home"
-//         component={Home}
-//         options={{
-//           tabBarLabel: 'Home',
-//           tabBarIcon: ({ color }) => (
-//             <MaterialCommunityIcons name="home" color={color} size={26} />
-//           ),
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Notification"
-//         component={Notification}
-//         options={{
-//           tabBarLabel: 'Updates',
-//           tabBarIcon: ({ color }) => (
-//             <MaterialCommunityIcons name="bell" color={color} size={26} />
-//           ),
-//         }}
-//       />
-//     </Tab.Navigator>
-//   );
-// };
+};
+const CustomTabBarBackground = () => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={[styles.tabBarBackground, { paddingBottom: insets.bottom }]}>
+      <LinearGradient
+        colors={['rgba(241, 202, 255, 0.8)', 'rgba(241, 202, 255, 0.3)']}
+        start={[0, 0]}
+        end={[1, 0]}
+        style={styles.gradient}
+      />
+    </View>
+  );
+};
